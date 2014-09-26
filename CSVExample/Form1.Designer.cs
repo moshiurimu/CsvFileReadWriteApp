b8807d0dd4b6783b60ccbeaaeb22488d3942b789
moshiurimu <moshiurimu@gmail.com>
2014-09-26 23:12:11 +0600

    Csv file read and write operation using list.

---
+++ CSVExample/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace CSVExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.homeContTextBox = new System.Windows.Forms.TextBox();
+            this.homeAddreTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.allPersonView = new System.Windows.Forms.ListView();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.searchButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(126, 22);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(320, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(470, 25);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(147, 51);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Contact Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Home contact number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Home address";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(127, 48);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(320, 20);
+            this.emailTextBox.TabIndex = 2;
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(127, 74);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(320, 20);
+            this.contactTextBox.TabIndex = 3;
+            // 
+            // homeContTextBox
+            // 
+            this.homeContTextBox.Location = new System.Drawing.Point(127, 100);
+            this.homeContTextBox.Name = "homeContTextBox";
+            this.homeContTextBox.Size = new System.Drawing.Size(320, 20);
+            this.homeContTextBox.TabIndex = 4;
+            // 
+            // homeAddreTextBox
+            // 
+            this.homeAddreTextBox.Location = new System.Drawing.Point(127, 126);
+            this.homeAddreTextBox.Name = "homeAddreTextBox";
+            this.homeAddreTextBox.Size = new System.Drawing.Size(320, 20);
+            this.homeAddreTextBox.TabIndex = 5;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(470, 91);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(147, 50);
+            this.showButton.TabIndex = 7;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // allPersonView
+            // 
+            this.allPersonView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.allPersonView.Location = new System.Drawing.Point(-1, 271);
+            this.allPersonView.Name = "allPersonView";
+            this.allPersonView.Size = new System.Drawing.Size(669, 204);
+            this.allPersonView.TabIndex = 8;
+            this.allPersonView.UseCompatibleStateImageBehavior = false;
+            this.allPersonView.View = System.Windows.Forms.View.Details;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(126, 35);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(320, 20);
+            this.searchTextBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Search";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(125, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(208, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Search using Name, Email or personal contact no.";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 133;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email Address";
+            this.columnHeader2.Width = 147;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Contact No";
+            this.columnHeader3.Width = 105;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Home Contact No";
+            this.columnHeader4.Width = 130;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Address";
+            this.columnHeader5.Width = 113;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(470, 19);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(147, 50);
+            this.searchButton.TabIndex = 11;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.homeAddreTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.homeContTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.contactTextBox);
+            this.groupBox1.Controls.Add(this.emailTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(641, 159);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entry Option :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.searchButton);
+            this.groupBox2.Controls.Add(this.searchTextBox);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(12, 177);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(641, 88);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search Option :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 475);
+            this.Controls.Add(this.allPersonView);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Personal Information Storing App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.TextBox homeContTextBox;
+        private System.Windows.Forms.TextBox homeAddreTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListView allPersonView;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ CSVExample/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSVLib;
+
+namespace CSVExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string fileLocation = @"namelist.csv";
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            FileStream FileStreamReader = new FileStream(fileLocation,FileMode.OpenOrCreate);
+            CsvFileReader aReader = new CsvFileReader(FileStreamReader);
+            List<string> personRecod = new List<string>();
+            
+                while (aReader.ReadRow(personRecod))
+                {
+                    if (personRecod[2]==contactTextBox.Text)
+                    {
+                        MessageBox.Show("Contact No is alredy exist!");
+                        FileStreamReader.Close();
+                        return;
+                    }
+                }
+            
+            FileStreamReader.Close();
+
+            FileStream FileStreamWriter= new FileStream(fileLocation, FileMode.Append);
+            CsvFileWriter aWriter = new CsvFileWriter(FileStreamWriter);
+            List<string> personList = new List<string>();
+            personList.Add(nameTextBox.Text);
+            personList.Add(emailTextBox.Text);
+            personList.Add(contactTextBox.Text);
+            personList.Add(homeContTextBox.Text);
+            personList.Add(homeAddreTextBox.Text);
+            aWriter.WriteRow(personList);
+            FileStreamWriter.Close();
+            MessageBox.Show("Add Sucsessfuly!");
+            Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+           FileStream aStream = new FileStream(fileLocation, FileMode.OpenOrCreate);
+            CsvFileReader aReader = new CsvFileReader(aStream);
+            List<string> personList = new List<string>();
+            allPersonView.Items.Clear();
+            while (aReader.ReadRow(personList))
+            {
+
+                ListViewItem items = new ListViewItem(personList[0]);
+                items.SubItems.Add(personList[1]);
+                items.SubItems.Add(personList[2]);
+                items.SubItems.Add(personList[3]);
+                items.SubItems.Add(personList[4]);
+                allPersonView.Items.Add(items);
+
+            }
+
+            aStream.Close();
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            FileStream aStream = new FileStream(fileLocation, FileMode.OpenOrCreate);
+            CsvFileReader aReader = new CsvFileReader(aStream);
+            List<string> personList = new List<string>();
+            allPersonView.Items.Clear();
+            while (aReader.ReadRow(personList))
+            {
+
+                if (personList[0] == searchTextBox.Text || personList[1] == searchTextBox.Text || personList[2] == searchTextBox.Text)
+                {
+
+                    ListViewItem items = new ListViewItem(personList[0]);
+                    items.SubItems.Add(personList[1]);
+                    items.SubItems.Add(personList[2]);
+                    items.SubItems.Add(personList[3]);
+                    items.SubItems.Add(personList[4]);
+                    allPersonView.Items.Add(items);
+                }
+            }
+
+            if (allPersonView.Items.Count==0)
+            {
+                MessageBox.Show("Dosn't match!");
+                searchTextBox.Text = "";
+            }
+
+            aStream.Close();
+        }
+
+        private void Clear()
+        { 
+            nameTextBox.Text= "";
+            emailTextBox.Text= "";
+            contactTextBox.Text= "";
+            homeContTextBox.Text= "";
+            homeAddreTextBox.Text = "";
+        }
+
+       
+    }
+}
